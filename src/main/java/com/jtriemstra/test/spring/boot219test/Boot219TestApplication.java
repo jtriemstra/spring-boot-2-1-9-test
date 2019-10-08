7031592485b0726a24c1843eac1b6f3feba31c04
Joel Triemstra <>
2019-10-08 19:53:08 -0400

    initial files from initializr

---
+++ src/main/java/com/jtriemstra/test/spring/boot219test/Boot219TestApplication.java
@@ -0,0 +1,13 @@
+package com.jtriemstra.test.spring.boot219test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot219TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot219TestApplication.class, args);
+	}
+
+}
